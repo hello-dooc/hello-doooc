12f8cfc8290737ea4649775a7385a23da7b5f2b8
hanmin <1799930668@qq.com>
2020-10-27 17:25:14 +0800

    2020-10-27 5:25pm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 
-import SelfInfo from './self/SelfInfo'
-
 export default class App extends Component {
   render() {
     return (
-      <SelfInfo></SelfInfo>
+      <div></div>
     )
   }
 }
