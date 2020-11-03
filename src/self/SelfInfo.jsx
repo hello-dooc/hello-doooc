139eaea8b73bf0cf8521cc37ed4ece38bcdc9484
hanmin <1799930668@qq.com>
2020-11-03 22:04:07 +0800

    11-03 10:04 pm

--- src/self/SelfInfo.jsx
+++ src/self/SelfInfo.jsx
@@ -1,22 +1,60 @@
 import React, { Component } from 'react';
-import { Descriptions } from 'antd';
+import axios from 'axios'
 import {withRouter} from 'react-router-dom'
 import {get} from '@u/http'
 
-import SelfInfoWrap from './StyledSelf'
+import { Upload, message } from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 
-import profile from '@a/images/timg.jpeg'
+import SelfInfoWrap from './StyledSelf'
+import UserInfo from './UserInfo'
+
+// import profile from '@a/images/timg.jpeg'
+function getBase64(img, callback) {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => callback(reader.result));
+    reader.readAsDataURL(img);
+  }
+function beforeUpload(file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+  }
 
 @withRouter
 class SelfInfo extends Component {
     constructor(props){
         super(props)
         this.state={
-            addressList:[]
+            addressList:[],
+            loading: false,
+            userInfo:[],
         }
-        this.handleGotoAddress=this.handleGotoAddress.bind(this)
     }
+    handleChange = info => {
+        if (info.file.status === 'uploading') {
+          this.setState({ loading: true });
+          return;
+        }
+        if (info.file.status === 'done') {
+          // Get this url from response in real world.
+          getBase64(info.file.originFileObj, imageUrl =>
+            this.setState({
+              imageUrl,
+              loading: false,
+            }),
+          );
+        }
+      };
+    
     async getAddressList(){
+        axios.defaults.headers.common["token"] = 'token_123456';
         let result = await get({
             url:'http://123.56.160.44:8080/user/address/list',
         })
@@ -35,32 +73,48 @@ class SelfInfo extends Component {
             addressList:arr
         })
     }
-    handleGotoAddress(list){
-        return ()=>{
-            this.props.history.push('/address',{list})
-        }
+    async getUserInfo(){
+        axios.defaults.headers.common["token"] = 'token_123456';
+        let result = await get({
+            url:'http://123.56.160.44:8080/user/info/detail'
+        })
+        let info = result.data.data;
+        let addr = info.addressInfo.province+info.addressInfo.city+info.addressInfo.country+info.addressInfo.addressDetail;
+        this.setState({
+            userInfo:{
+                ...info,
+                addr
+            }
+        })
     }
     componentDidMount(){
         this.getAddressList()
+        this.getUserInfo()
     }
+    
     render() {
+        const { loading, imageUrl } = this.state;
+        const uploadButton = (
+        <div>
+            {loading ? <LoadingOutlined /> : <PlusOutlined />}
+            <div style={{ marginTop: 8 }}>Upload</div>
+        </div>
+        );
         return (
             <SelfInfoWrap id="self">
                 <div className="self-wrap"> 
-                    <Descriptions bordered>
-                        <Descriptions.Item span={3}>
-                            <img src={profile} alt=""/>
-                        </Descriptions.Item>
-                        <Descriptions.Item label="用户名" span={3}></Descriptions.Item>
-                        <Descriptions.Item label="手机号" span={3}></Descriptions.Item>
-                        <Descriptions.Item label="性别" span={3}></Descriptions.Item>
-                        <Descriptions.Item label="邮箱" span={3}></Descriptions.Item>
-                        <Descriptions.Item label="QQ" span={3}></Descriptions.Item>
-                        <Descriptions.Item label="收货地址" span={3}>
-                            <span>北京市昌平区沙河镇沙阳路18号北京科技职业技术学院</span>
-                            <button onClick={this.handleGotoAddress(this.state.addressList)}>选择默认地址</button>
-                        </Descriptions.Item>
-                    </Descriptions>
+                    <Upload
+                        name="avatar"
+                        listType="picture-card"
+                        className="avatar-uploader"
+                        showUploadList={false}
+                        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                        beforeUpload={beforeUpload}
+                        onChange={this.handleChange}
+                    >
+                        {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+                    </Upload>
+                    <UserInfo userInfo={this.state.userInfo}></UserInfo>
                 </div>
             </SelfInfoWrap>
         );
