b9070d921140df8b5c17b88858849005a814a0ae
lyx <1092336913@qq.com>
2020-11-11 19:04:41 +0800

    11.11

--- src/home/food/ui/List.jsx
+++ src/home/food/ui/List.jsx
@@ -5,30 +5,29 @@ import list1 from '@a/images/cadfc367d9263412d9ac9356f53f3b97.jpg'
 import { Pagination } from 'antd';
 
 const List = (props) => {
-  console.log(props)
   return (
         <div className="center">
           <ol>
             {
               props.list.length>0 && props.list.map(item => {
                   return(
-                    <li key={item.clothesId}>
-                      <img src={item.clothesImage} alt=""/>
+                    <li key={item.id}>
+                      <img src={item.image} alt=""/>
                       <p>
-                        <span>{item.clothesDiscount}</span>
-                        <span className="title">{item.clothesName}</span>
+                        <span>满99减20</span>
+                        <span className="title">{item.name}</span>
                       </p>
-                      <p className="price">￥{item.clothesPrice}</p>
+                      <p className="price">￥{item.originalPrice}</p>
                       <p>
-                        <del>￥{item.clothesPrice}</del>
-                        <del>已售{item.clothesCount}</del>
+                        <del>￥{item.salePrice}</del>
+                        <del>已售{item.sales}</del>
                       </p>
                     </li>
                   )
               })
             }
           </ol>
-          <Pagination defaultCurrent={1} total={50} />
+          
         </div>
       ) 
 }
